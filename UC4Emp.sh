eccce18b3416d15aee83314479a8044407ac8d8b
Kavyasree-J-Ganesh <kavyasreejganesh97@gmail.com>
2022-10-30 17:55:28 +0530

    [Kavyasree J Ganesh]-EmployeePracticeProblem

---
+++ UC4Emp.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -x
+
+isPartTime=1;
+isFullTime=2
+empCheck=$((RANDOM%3));
+empRatePerHr=20;
+
+case $empCheck in
+    $isFullTime)
+        empHrs=8
+        ;;
+    $isPartTime)
+        empHrs=4
+        ;;
+    *)
+        empHrs=0
+esac
+salary=$(($empHrs*$empRatePerHr))
